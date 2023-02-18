cfab36981304ab016eec6f4eaa5ab8d650378e3a
Jose Neves <contato@joseneves.com.br>
2023-02-18 11:04:48 -0300

    Initial project setup

---
+++ BasicsKit/BasicsKit.h
@@ -0,0 +1,18 @@
+//
+//  BasicsKit.h
+//  BasicsKit
+//
+//  Created by Jose de Arimateia Neves Junior on 18/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasicsKit.
+FOUNDATION_EXPORT double BasicsKitVersionNumber;
+
+//! Project version string for BasicsKit.
+FOUNDATION_EXPORT const unsigned char BasicsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicsKit/PublicHeader.h>
+
+
